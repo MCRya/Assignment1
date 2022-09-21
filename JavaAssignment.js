e9be7deb9cfc73bfa533b5d8d05a31e9466ea404
MCRya <112865331+MCRya@users.noreply.github.com>
2022-09-21 18:52:48 +0100

    Add files via upload

--- JavaAssignment.js
+++ JavaAssignment.js
@@ -1,17 +1,60 @@
-const PI=3.14
+const PI = 3.14;
 
-let r=45
+//Circle area
+var r = 5;
 
-let a = (PI*r**2)
+var area = (PI*r)^2;
+console.log("Circle Area = " + area);
 
-console.log(a)
+//Circle circumference
+var r = 8
 
-r=6
+var circumference = 2*PI*r;
+console.log("Circle Circumference = " + circumference);
 
-let c = (2*PI*r)
+//Rectangle area
+var l = 7;
+var w = 4;
 
-console.log(c)
+var area = l*w;
+console.log("Rectangle area = " + area);
 
-let l=5
+//Triangle area
+var b = 11;
+var h = 5;
 
-a=(L*W)
+var area = (b*h);
+area = area * 0.5;
+console.log("Triangle Area = " + area);
+
+//Equilateral Triangle Area 
+var a = 4;
+var b = 11;
+
+var asq = (a^2);
+var bsq = (b^2);
+var csq = (asq + bsq) / 2;
+var area = csq
+console.log("Equilateral Triangle Area = " + area) // Wrong answer i think. 
+
+//3D Rectangle
+var l = 10;
+var w = 13;
+var h = 8;
+
+var volume = (l*w*h);
+console.log("3D Rectangle = " + volume);
+
+//3D Cylinder
+var r = 12
+var h = 5
+
+var volume = (PI*(r^2)*h);
+console.log("3D Cylinder = " + volume);
+
+//3D Sphere
+var r = 4;
+
+var volume = PI*(r^3);
+volume = volume * (4/3);
+console.log("Sphere volume = " + volume);
